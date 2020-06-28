0181b7d68bab795d55c6bceb9f67390abc3458e3
Ananya Tripathy <TripathyAnanya199@github.com>
2020-06-28 21:20:54 +0530

    Initial commit

---
+++ .idea/.name
@@ -0,0 +1,1 @@
+Ludo Dice Roller
---
+++ app/src/main/java/com/example/ludodiceroller/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ludodiceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonClick(view: View) {
+        val randomnumber=(1..6).random()
+        textView.text=randomnumber.toString()
+    }
+}
